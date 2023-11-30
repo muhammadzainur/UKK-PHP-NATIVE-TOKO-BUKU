db4983f1c73630fd2c1f1846d84f3f3a4c918eed
Web Programmer <133774248+muhammadzainur@users.noreply.github.com>
2023-11-30 18:55:40 +0700

    Update update_penjualan_proses.php

--- pages/penjualan/update_penjualan_proses.php
+++ pages/penjualan/update_penjualan_proses.php
@@ -1,12 +1,10 @@
 <?php
 include "../../conf/conn.php";
 $id = $_POST['id'];
-$buku = $_POST['buku'];
 $kasir = $_POST['kasir'];
-$jumlah = $_POST['jumlah'];
 $total = $_POST['total'];
 $tanggal = $_POST['tanggal'];
-$query = ("UPDATE penjualan SET id_buku='$buku',id_kasir='$kasir',jumlah='$jumlah', total='$total', tanggal='$tanggal' WHERE id_penjualan ='$id'");
+$query = ("UPDATE penjualan SET id_kasir='$kasir', total='$total', tanggal='$tanggal' WHERE id_penjualan ='$id'");
 if ($koneksi->query($query)) {
   //redirect ke halaman index.php 
   //header("location: index.php");
